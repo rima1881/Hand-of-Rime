875b02bdaefd26cb7ce97d756e1bfcddb2276cf6
Amir <amd.gharibipour@outlook.com>
2024-11-08 15:49:30 -0500

    Update simulation.py
    replaced old shitty code

--- simulation.py
+++ simulation.py
@@ -9,30 +9,11 @@ from pydrake.all import StartMeshcat
 from pydrake.all import AddDefaultVisualization
 from pydrake.all import Solve
 from pydrake.all import RigidTransform
+from pydrake.all import RandomGenerator
 import numpy as np
 
 from pydrake.multibody.inverse_kinematics import InverseKinematics
 
-"""
-
-urdf_path = "robot.v3.model.urdf"
-
-f = open(urdf_path, "r")
-robot_urdf_string = f.read()
-robot_urdf_string.replace("\n","")
-
-# Visualize the cylinder from the SDFormat string you just defined.
-visualizer = ModelVisualizer(meshcat=meshcat)
-visualizer.parser().AddModelsFromString(robot_urdf_string, "urdf")
-
-test_mode = True if "TEST_SRCDIR" in os.environ else False
-
-
-# Click the "Stop Running" button in MeshCat when you're finished.
-visualizer.Run(loop_once=test_mode)
-
-
-"""
 
 #   -----------------------------------------------------
 #   Loading robot and building the world
@@ -52,8 +33,8 @@ urdf_path = "robot.v3.model.urdf"
 
 f = open(urdf_path, "r")
 robot_urdf_string = f.read()
-robot_urdf_string.replace("\n","")
-Parser(plant).AddModelsFromString(robot_urdf_string , "urdf")
+robot_urdf_string.replace("\n", "")
+Parser(plant).AddModelsFromString(robot_urdf_string, "urdf")
 
 robot_base = plant.GetFrameByName("meca_base_link")
 plant.WeldFrames(
@@ -69,7 +50,6 @@ AddDefaultVisualization(builder=builder, meshcat=meshcat)
 diagram = builder.Build()
 #   -------------------------------------------------------------
 #   Creating Simulation
-#   We won't run it yet
 
 simulator = Simulator(diagram)
 simulator_context = simulator.get_context()
@@ -77,62 +57,10 @@ simulator_context = simulator.get_context()
 plant_context = plant.GetMyMutableContextFromRoot(simulator_context)
 
 #   ------------------------------------------------------------
-#   Inverse Kinematic
-IK = InverseKinematics(plant , plant_context)
 
-prog = IK.prog()
-q = IK.q()
-
-p_AQ_lower = np.array([-50, 0, -50])
-p_AQ_upper = np.array([50,   0.01, 50])
-
-
-desired_orientation = RotationMatrix(np.eye(3))  # Target orientation as an identity rotation (no rotation)
-
-print(desired_orientation)
-
-IK.AddOrientationConstraint(
-    plant.world_frame(),
-    desired_orientation,
-    plant.GetFrameByName("meca_axis_6_link"),
-    desired_orientation,
-    0.01
-)
-
-
-IK.AddPositionConstraint(
-    plant.GetFrameByName("meca_axis_6_link"),
-    [0, 0, 0],
-    plant.world_frame(),
-    p_AQ_lower,
-    p_AQ_upper
-)
-
-result = Solve(prog)
-
-new_q = result.GetSolution(q)
-
-print(new_q)
-
-if result.is_success():
-    print("yay")
-else:
-    print("ahhhh")
-
-
-def generateRandomJointValue( plant):
-
-    upperLimits = plant.GetPositionUpperLimits()
-    lowerLimits = plant.GetPositionLowerLimits()
-
-    random_q = np.random.uniform(low=lowerLimits, high=upperLimits)
-    print(np.degrees(random_q))
-    return random_q
-random_pos = generateRandomJointValue(plant)
-
-#plant.SetPositions( plant_context , np.array([ -5.26315790e-12 , -2.41441875e-01  , 2.30523678e-01 , 0 , 5.24514380e-01 ,  0]))
-#plant.SetPositions( plant_context , np.array([ 3.04676705 , 0.29143209 , -0.02131716 , -1.97426958 , -1.99391916 ,  0.61141654]))
-#plant.SetPositions( plant_context , np.deg2rad([0.0, -13.612000465, 12.754300117, 0.0, 30.857599258, 0.0]))
+plant.SetPositions(plant_context, np.array(
+    [-0.,   -0.237575,      0.22260574,     3.,     1.53856492,     0.]
+))
 
 end_effector_transform = plant.CalcRelativeTransform(
         plant_context,
@@ -153,11 +81,8 @@ simulator.set_target_realtime_rate(1.0)
 
 meshcat.StartRecording()
 
-
-
 while True:
     # Adjust joint values dynamically if needed
-    # plant.SetPositions(plant_context, new_joint_values)
 
     # Advance the simulation in small time steps
     simulator.AdvanceTo(simulator.get_context().get_time() + 0.01)
